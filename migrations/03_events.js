6c23488ab2094d3d45acb677bae27b8abfe32d8e
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-10-29 11:41:46 -0600

    "Events seeds up and running. Some changes in migrations"

--- migrations/03_events.js
+++ migrations/03_events.js
@@ -1,29 +1,26 @@
-
-exports.up = function(knex, Promise) {
-return knex.schema.createTable('events', function(table) {
- // TABLE COLUMN DEFINITIONS HERE
- table.increments()
- table.string('eventName', 255).notNullable().defaultTo('')
- table.integer('platform_id')
- table.foreign('platform_id').references('platforms.id')
- table.integer('host_id')
- /*   We need to figure out where we are going to reference this..
- table.foreign('host_id').references('hosts')
- */
- table.string('address', 255)
- table.string('city', 100)
- table.integer('zip', 255)
- table.string('link', 144)
- table.date('date').notNullable()
- table.time('start').notNullable()
- table.time('end').notNullable()
- table.string('description', 255)
- table.integer('playerLimit')
- table.integer('ageLimit')
- table.boolean('privacy').notNullable()
- table.timestamps(true, true)
-})
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('events', function (table) {
+    // TABLE COLUMN DEFINITIONS HERE
+    table.increments()
+    table.string('eventName', 255).notNullable().defaultTo('')
+    table.integer('platform_id')
+    table.foreign('platform_id').references('platforms.id').onDelete('CASCADE')
+    table.integer('host_id').notNullable()
+    table.foreign('host_id').references('users.id').onDelete('CASCADE')
+    table.string('address', 255)
+    table.string('city', 100)
+    table.integer('zip', 255)
+    table.string('link', 144)
+    table.date('date').notNullable()
+    table.timestamp('start').notNullable()
+    table.timestamp('end').notNullable()
+    table.string('description', 255)
+    table.integer('playerLimit')
+    table.integer('ageLimit')
+    table.boolean('privacy').notNullable()
+    table.timestamps(true, true)
+  })
 }
-exports.down = function(knex, Promise) {
-return knex.schema.dropTableIfExists('events')
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('events')
 }
---
+++ seeds/003_events.js
@@ -0,0 +1,59 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('events').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('events').insert([{
+            id: 1,
+            eventName: `Stink's Super Smash Beatdown`,
+            platform_id: 9,
+            host_id: 1,
+            address: `3700 Hayden Place #2`,
+            city: `Boulder`,
+            zip: 80301,
+            date: new Date(11, 15, 2018),
+            start: new Date(2018, 11, 15, 7, 30, 0, 0),
+            end: new Date(2018, 11, 15, 9, 30, 0, 0),
+            description: "We are gonna play some SMASH BROS on the PLAYSTATION 4! SAY WHAAAT?",
+            playerLimit: 4,
+            ageLimit: 18,
+            privacy: false,
+            created_at: new Date(2018, 10, 28, 9, 30, 0, 0),
+            updated_at: new Date(2018, 10, 28, 9, 30, 0, 0)
+          },
+          {
+            id: 2,
+            eventName: `John's Mario Party`,
+            platform_id: 5,
+            host_id: 2,
+            link: "www.johnsmarioparty.com",
+            date: new Date(11, 4, 2018),
+            start: new Date(2018, 11, 4, 3, 30, 0, 0),
+            end: new Date(2018, 11, 4, 9, 30, 0, 0),
+            description: "All I do is party!",
+            privacy: true,
+            created_at: new Date(2018, 10, 28, 9, 30, 0, 0),
+            updated_at: new Date(2018, 10, 28, 9, 30, 0, 0)
+          },
+          {
+            id: 3,
+            eventName: `Monopoly Marathon`,
+            platform_id: 16,
+            host_id: 3,
+            address: `YMCA on 28th`,
+            city: `Boulder`,
+            date: new Date(11, 4, 2018),
+            start: new Date(2018, 11, 1, 15, 30, 0, 0),
+            end: new Date(2018, 11, 1, 17, 30, 0, 0),
+            description: "No matter how long it takes, the game always ends with the table getting flipped.",
+            privacy: false,
+            created_at: new Date(2018, 10, 28, 9, 30, 0, 0),
+            updated_at: new Date(2018, 10, 28, 9, 30, 0, 0)
+          }
+        ])
+        .then(function () {
+          // Moves id column (PK) auto-incrementer to correct value after inserts
+          return knex.raw("SELECT setval('events_id_seq', (SELECT MAX(id) FROM events))")
+        })
+    })
+}

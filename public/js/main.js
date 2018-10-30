bbcf8180c13d072b1a181dcc14a7cbefdb837907
Josh <grafton.josh@gmail.com>
2018-10-30 15:44:13 -0600

    Added basic front end for events routes

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,105 @@
-/* TESTING VARIABLES */
-let tmpArr = [1,2,3,4,5,6,7,8,9,10]
-/* TESTING VARIABLES */
-
-let counter = 0
-
-document.addEventListener('DOMContentLoaded', function() {
-  let body = document.getElementsByTagName('body')
-  // body.addEventListener('onresize', defineConstraints())
-  M.AutoInit();
-  // NOTE: tmpArr is res data
-  populateParallax(tmpArr)
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Connected to Main.js')
+  getEvents()
 })
 
-function defineConstraints (){
-  let headerImg = document.getElementById('movBackground')
-  let headerBar = document.getElementById('headerBar')
-  let height = headerBar.clientHeight
-  let width = headerBar.clientWidth
-  console.log(height, width)
-  headerImg.height = height
-  headerImg.width = width
-}
-// function clears parallax and repopulates
-function buildAndBurnParallax (arr){
-  let parCont = document.getElementById('parallax_container')
-  parCont.innerHTML = ""
-  counter = 0
-  populateParallax(arr)
-}
-// function populates parallax and reinitializes it
-function populateParallax (arr){
-  console.log("populateParallax")
-  let templateHead = `<div class="parallax-container"><div class="parallax"><img src="https://g.foolcdn.com/editorial/images/453677/mans-hands-holding-a-video-game-controller.jpg"></div></div>`
+// /* TESTING VARIABLES */
+// let tmpArr = [1,2,3,4,5,6,7,8,9,10]
+// /* TESTING VARIABLES */
+//
+// let counter = 0
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//   let body = document.getElementsByTagName('body')
+//   // body.addEventListener('onresize', defineConstraints())
+//   M.AutoInit();
+//   // NOTE: tmpArr is res data
+//   populateParallax(tmpArr)
+// })
+//
+// function defineConstraints (){
+//   let headerImg = document.getElementById('movBackground')
+//   let headerBar = document.getElementById('headerBar')
+//   let height = headerBar.clientHeight
+//   let width = headerBar.clientWidth
+//   console.log(height, width)
+//   headerImg.height = height
+//   headerImg.width = width
+// }
+// // function clears parallax and repopulates
+// function buildAndBurnParallax (arr){
+//   let parCont = document.getElementById('parallax_container')
+//   parCont.innerHTML = ""
+//   counter = 0
+//   populateParallax(arr)
+// }
+// // function populates parallax and reinitializes it
+// // function populateParallax (arr){
+// //   console.log("populateParallax")
+// //   let templateHead = `<div class="parallax-container"><div class="parallax"><img src="https://g.foolcdn.com/editorial/images/453677/mans-hands-holding-a-video-game-controller.jpg"></div></div>`
+// //
+// //   let templateBody = `<div class="section white"><div class="row container"><h2 class="header pHeader">Event Title</h2><p class="grey-text text-darken-3 lighten-3 pPlat">Platform</p><p class="grey-text text-darken-3 lighten-3 pDateTime">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>    <br><p class="grey-text text-darken-3 lighten-3 pdesc">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p></div><div class="row container"><div class="col s10 m10 l10"></div><div class="col s2 m2 l2"><a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a></div> </div></div><div class="parallax-container"><div class="parallax"><img src="https://g.foolcdn.com/editorial/images/453677/mans-hands-holding-a-video-game-controller.jpg"></div></div>`
+// //   let parCont = document.getElementById('parallax_container')
+// //   parCont.innerHTML += templateHead
+// //   for(let i = 0; i < arr.length; i++){
+// //     parCont.innerHTML += templateBody
+// //     counter++
+// //   }
+// //   initParallax()
+// // }
 
-  let templateBody = `<div class="section white"><div class="row container"><h2 class="header pHeader">Event Title</h2><p class="grey-text text-darken-3 lighten-3 pPlat">Platform</p><p class="grey-text text-darken-3 lighten-3 pDateTime">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>    <br><p class="grey-text text-darken-3 lighten-3 pdesc">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p></div><div class="row container"><div class="col s10 m10 l10"></div><div class="col s2 m2 l2"><a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a></div> </div></div><div class="parallax-container"><div class="parallax"><img src="https://g.foolcdn.com/editorial/images/453677/mans-hands-holding-a-video-game-controller.jpg"></div></div>`
-  let parCont = document.getElementById('parallax_container')
-  parCont.innerHTML += templateHead
-  for(let i = 0; i < arr.length; i++){
-    parCont.innerHTML += templateBody
-    counter++
+function getEvents() {
+  const contentArea = document.getElementById('parallax_container')
+axios.get('http://localhost:3000/events')
+    .then((response) => {
+      response.data.forEach((event) => {
+
+      let tbody = document.querySelector('#list-movies tbody')
+      let tr = document.createElement('tr')
+      let eventName = document.createElement('td')
+      let description = document.createElement('td')
+      let del_td = document.createElement('td')
+      let deleteButton = document.createElement('button')
+      let edit_td= document.createElement('td')
+      let editButton = document.createElement('button')
+
+      eventName.innerText = event.eventName
+      description.innerText = event.description
+      deleteButton.innerText =  'Delete'
+      editButton.innerText = `Edit`
+
+      deleteButton.setAttribute('data-id', event.id)
+      deleteButton.setAttribute('class', 'waves-effect waves-light btn')
+
+      editButton.setAttribute('data-id', event.id)
+      editButton.setAttribute('class', 'waves-effect waves-light btn')
+
+      deleteButton.addEventListener('click', (ev) => {
+        let eventId = ev.target.getAttribute('data-id')
+
+        // DELETE THIS RECORD!
+        axios.delete(`/events/${eventId}`)
+        .then((response) => {
+          del_td.parentElement.remove()
+        })
+        .catch((err) => {
+          //console.log(err)
+        })
+      })
+
+      //Append to page
+      tr.appendChild(eventName)
+      tr.appendChild(description)
+      tr.appendChild(del_td)
+      del_td.appendChild(deleteButton)
+      tr.appendChild(edit_td)
+      edit_td.appendChild(editButton)
+      tbody.appendChild(tr)
+
+      })
+    })
   }
-  initParallax()
-}
+
 // fill titles
 // fill description
 // fill datetime

aaf199e2c39133a7da90b0f86ca613207eaded49
Josh <grafton.josh@gmail.com>
2018-11-01 14:18:37 -0600

    all forms on one page

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,93 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Connected to Main.js')
   getEvents()
-
+  handleCreateForm()
+  handleEditForm()
 
 })
 
+function handleEditForm() {
+  let form = document.getElementById('edit-event')
+  form.addEventListener('submit', (ev) => {
+    console.log('Submitted');
+    console.log(('EVENT TARGET>>>', event.target.elements[0].name));
+    // grab all values from the form
+    let postData = {}
+    let formElements = ev.target.elements
+
+    for (var i = 0; i < formElements.length; i++) {
+      let inputName = formElements[i].name
+      if( inputName ) {
+        postData[inputName] = formElements[i].value
+      }
+    }
+    postData.end = `${postData.end}:00.000Z`
+    postData.start = `${postData.start}:00.000Z`
+
+    console.log('postData', postData);
+
+    // axios.post that data to the correct backend route
+    axios.put('/events/6', postData)
+    .then((response) => {
+      console.log(`response:`,response);
+      // document.getElementById("submit-form").disabled = true
+      // let success = document.createElement('p')
+      //     success.innerHTML = `Successfully added ${response.data[0].title}.<a href='events.html'>See all events.</a>`
+      //     form.appendChild(success)
+    })
+    .catch((error) => {
+      console.log(`error happened`);
+      console.log(error)
+    })
+  })
+}
+
+
+function handleCreateForm() {
+  let form = document.getElementById('create-event')
+  form.addEventListener('submit', (ev) => {
+    console.log('Submitted');
+    ev.preventDefault()
+    console.log(('EVENT TARGET>>>', event.target.elements[0].name));
+    // grab all values from the form
+    let postData = {}
+    let formElements = ev.target.elements
+
+    for (var i = 0; i < formElements.length; i++) {
+      let inputName = formElements[i].name
+      if( inputName ) {
+        postData[inputName] = formElements[i].value
+      }
+    }
+    postData.end = `${postData.end}:00.000Z`
+    postData.start = `${postData.start}:00.000Z`
+
+    console.log('postData', postData);
+
+    // axios.post that data to the correct backend route
+    axios.post('/events', postData)
+    .then((response) => {
+      response.redirect('')
+      console.log(`response:`,response);
+      // document.getElementById("submit-form").disabled = true
+      // let success = document.createElement('p')
+      //     success.innerHTML = `Successfully added ${response.data[0].title}.<a href='events.html'>See all events.</a>`
+      //     form.appendChild(success)
+    })
+    .catch((error) => {
+      console.log(`error happened`);
+      console.log(error)
+    })
+  })
+}
+
 function getEvents() {
-  const contentArea = document.getElementById('parallax_container')
+  const contentArea = document.getElementById('table')
   axios.get('/events')
     .then((response) => {
       response.data.forEach((event) => {
 
-        let tbody = document.querySelector('#list-movies tbody')
+        let tbody = document.querySelector('#list-events tbody')
         let tr = document.createElement('tr')
         let eventName = document.createElement('td')
         let description = document.createElement('td')
@@ -51,6 +127,7 @@ function getEvents() {
               //console.log(err)
             })
         })
+
         joinButton.addEventListener('click', (ev) => {
           let eventId = ev.target.getAttribute('data-id')
           console.log(eventId);
@@ -71,6 +148,8 @@ function getEvents() {
         del_td.appendChild(deleteButton)
         tr.appendChild(join_td)
         join_td.appendChild(joinButton)
+        tr.appendChild(edit_td)
+        edit_td.appendChild(editButton)
         tbody.appendChild(tr)
 
 
